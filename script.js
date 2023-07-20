db7be1e5f6b966d2c0dd2e2209aff5b945033bff
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-07-20 18:38:44 +0530

    fdysdgf

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ const modalHTML = `<div class="modal-container">
         <option value="TODO">TO DO</option>
         <option value="IN_PROGRESS">IN PROGRESS</option>
         <option value="DONE">DONE</option>
-        <textarea name="description" cols="30" rows="10" ></textarea>
-        <button>Create</button>
     </select>
+        <textarea name="description" cols="30" rows="10" ></textarea>
+        <button class="submit-button">Create</button>
+    
 </form>
 </div>`
 
@@ -29,26 +30,55 @@ function listener() {
 
     const crossButton = document.getElementsByClassName("cross")[0];
     crossButton.addEventListener("click", listener2)
-    
-    function listener2()
-    {
+
+    function listener2() {
         modalContainer.remove();
     }
 
     const form = document.getElementById("create-form");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
+    form.addEventListener("submit", formListener);
+
+    function formListener(event) {
+        event.preventDefault();
         let obj = {};
-        let children = e.target.children;
-        for(let i=0;i<children.length;i++)
-        {
-            if(children[i].name)
-            {
+        let children = event.target.children;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i].name) {
                 obj[children[i].name] = children[i].value;
             }
-               
-          
         }
-        
-    })
+        createTask(obj);
+        modalContainer.remove();
+        form.removeEventListener("submit", formListener);
+    }
+}
+
+// {/* <div class="task" draggable="true">
+//     <h3>Title</h3>
+//     <strong class="assinee-name">Tanmay Jain</strong>
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aliquid eveniet magni repellat nobis.
+//     </p>
+// </div> */}
+
+function createTask(obj) {
+    console.log(obj);
+    const task = document.createElement("div");
+    task.className = "task";
+    task.draggable = "true";
+    task.innerHTML = `<h4>${obj.title}</h4>
+    <b class="assinee-name">${obj.assignee}</b>
+    <p>${obj.description}</p>`
+
+    const status = document.getElementById(obj.status);
+    status.appendChild(task);
+}
+
+
+
+let openBox = false;
+function toggleProjectDropDown()
+{
+    const projectDropdownList = document.getElementsByClassName("dropdown-list")[0];
+    projectDropdownList.style.display = (projectDropdownList.style.display == "none") ? (projectDropdownList.style.display = "block") : (projectDropdownList.style.display = "none");
+
 }
